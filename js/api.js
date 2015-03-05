bd138fcaa6e7a8fde8b6ca42faaf6d44a428d7a5
formula1 <samtobia@gmail.com>
2015-03-04 18:29:50 -0800

    debugging

--- js/api.js
+++ js/api.js
@@ -10458,6 +10458,7 @@ AuthProvider.prototype.parseURL = function(){
     return this.fail("not expecting to login");
   }
   this.info = JSON.parse(this.info);
+  console.log(this.info);
   if(this.info.access_token){
     return this.finish();
   }
